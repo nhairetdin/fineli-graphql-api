360f994ae43aa9d3f6758d3746d240d0d1ae229e
Nico Hairetdin <hairetdin.nico@gmail.com>
2019-08-19 13:06:22 +0300

    Add foodnameContains field for FoodWhere, makes it possible to search foods by keyword

--- src/graphql/food/food.query.js
+++ src/graphql/food/food.query.js
@@ -1,6 +1,28 @@
 import { resolver } from 'graphql-sequelize'
 import { Food } from '../../models'
+import { Sequelize } from 'sequelize';
+import constructWhere from '../tools/process-arguments';
+
+const Op = Sequelize.Op
+
+const conditionsMap = {
+  foodnameContains: {
+    symbol: Op.like,
+    target: 'foodname',
+    inputType: 'string'
+  }
+}
 
 export const Query = {
-  food: resolver(Food)
+  food: resolver(Food, {
+    before: async (findOptions, args) => {
+      if (findOptions.where) {
+        findOptions.where = constructWhere(findOptions, args, conditionsMap)
+      }
+      return findOptions
+    },
+    after: async result => {
+      return result
+    }
+  })
 }
