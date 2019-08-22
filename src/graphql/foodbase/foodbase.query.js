06b5248e03200cd11718a68e02b478bc62940a80
Nico Hairetdin <hairetdin.nico@gmail.com>
2019-08-22 09:05:02 +0300

    Add foodnameContains filter for foodbase resolver

--- src/graphql/foodbase/foodbase.query.js
+++ src/graphql/foodbase/foodbase.query.js
@@ -1,7 +1,27 @@
 import { resolver } from 'graphql-sequelize'
 import { Foodbase } from '../../models'
-// import { Sequelize } from 'sequelize'
+import { Sequelize } from 'sequelize'
+import constructWhere from '../tools/process-arguments'
 
+const Op = Sequelize.Op
+
+const conditionsMap = {
+  foodnameContains: {
+    symbol: Op.like,
+    target: "foodname",
+    inputType: 'string'
+  }
+}
 export const Query = {
-  foodbase: resolver(Foodbase)
+  foodbase: resolver(Foodbase, {
+    before: async (findOptions, args) => {
+      if (findOptions.where) {
+        findOptions.where = constructWhere(findOptions, args, conditionsMap)
+      }
+      return findOptions
+    },
+    after: async result => {
+      return result
+    }
+  })
 }
